a8e1c92959aff98ca2d54be33dfd814c2743c732
Andrew Marfo <andrewmarfo23@gmail.com>
2024-02-11 20:30:00 +0000

    initializing firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 import './screens/chat_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(const MyApp());
 }
 
