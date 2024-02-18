121c7fee15341bcf7a1c56ea3316663183e0c52f
Andrew Marfo <andrewmarfo23@gmail.com>
2024-02-18 23:35:29 +0000

    displayin user names

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -15,41 +15,38 @@ class MessageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
-        Text(
-          userName,
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
+        Container(
+          width: 140,
+          decoration: BoxDecoration(
+            color: isMe ? Theme.of(context).primaryColor : Colors.pink,
+            borderRadius: BorderRadius.only(
+              topLeft: const Radius.circular(14),
+              topRight: const Radius.circular(14),
+              bottomLeft:
+                  !isMe ? const Radius.circular(0) : const Radius.circular(14),
+              bottomRight:
+                  isMe ? const Radius.circular(0) : const Radius.circular(14),
+            ),
           ),
-        ),
-        Row(
-          mainAxisAlignment:
-              isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
-          children: [
-            Container(
-              width: 140,
-              decoration: BoxDecoration(
-                color: isMe ? Theme.of(context).primaryColor : Colors.pink,
-                borderRadius: BorderRadius.only(
-                  topLeft: const Radius.circular(14),
-                  topRight: const Radius.circular(14),
-                  bottomLeft: !isMe
-                      ? const Radius.circular(0)
-                      : const Radius.circular(14),
-                  bottomRight: isMe
-                      ? const Radius.circular(0)
-                      : const Radius.circular(14),
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+          margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+          child: Column(
+            children: [
+              Text(
+                userName,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-              margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-              child: Text(
+              Text(
                 message,
                 style: const TextStyle(color: Colors.white),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ],
     );
