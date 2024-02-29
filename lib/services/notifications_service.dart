bf9a4f65efdcd0d8c4e5f09235db7950a3cb91a4
Andrew Marfo <andrewmarfo23@gmail.com>
2024-02-29 23:46:32 +0000

    creating a notification service

---
+++ lib/services/notifications_service.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'package:flutter/services.dart';
+
+class NotificationService {
+  static const MethodChannel _channel =
+      MethodChannel('com.example.my_app/notification_service');
+
+  NotificationService() {
+    _channel.setMethodCallHandler(_methodCallHandler);
+  }
+
+  Future<String?> initialize() async {
+    return await _channel.invokeMethod('initialize');
+  }
+
+  Future<void> requestPermissions() async {
+    return await _channel.invokeMethod('requestPermissions');
+  }
+
+  Future<void> configureSelectNotificationSubject(String subject) async {
+    return await _channel
+        .invokeMethod('configureSelectNotificationSubject', <String, dynamic>{
+      'subject': subject,
+    });
+  }
+
+  Future<dynamic> _methodCallHandler(MethodCall call) async {
+    switch (call.method) {
+      case 'selectNotification':
+        final String? subject = call.arguments['subject'];
+        // Handle the notification
+        break;
+      default:
+        throw PlatformException(
+            code: 'Unimplemented',
+            details:
+                'NotificationService does not implement method ${call.method}');
+    }
+  }
+}
