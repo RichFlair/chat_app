b2da035436bf7b604171ca5328358c44b5749b33
Andrew Marfo <andrewmarfo23@gmail.com>
2024-03-04 23:24:12 +0000

    creating a firebase messaging api

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './api/firebase_api.dart';
 import 'package:chat_app/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -6,9 +7,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 import './screens/auth_screen.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseApi().initFirebase;
   runApp(const MyApp());
 }
 
